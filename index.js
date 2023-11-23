cc1a60dfbb1c7e51482fd603f4b960987b810f4e
gabriel lima <gabriellima140806@gmail.com>
2023-11-22 21:48:33 -0300

    primeira execuçao do servidor

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const express require = require("expresss")
+const express = require("express")
+
 const app = express()
+
+app.get('/',(requisiçao, resposta)=>{
+    resposta.send("santos!")
+})
+
+app.listen(3000, ()=> {
+    console.log("servidor rodando na porta 3000!")
+})
