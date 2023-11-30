d8484bc88dba6be2600b9c3284ebe222c30bd8c6
gabriel lima <gabriellima140806@gmail.com>
2023-11-30 02:42:14 -0300

    iniciando leiturado dos dados

--- index.js
+++ index.js
@@ -32,6 +32,23 @@ app.post('/criar', (requisicao, reposta)=>{
 })
 
 app.get('/',(requisiçao, resposta)=>{
+    const sql = 'SELECT * FROM tarefas'
+    conexao.query(sql, (erro, dados)=>{
+        if (erro) {
+            return console.log(erro)
+        }
+        
+    const tarefas = dados.map((dado)=>{
+        return{
+            id: dado.id,
+            descricao: dado.descricao,
+            completa: dado.completa === 0 ? false : true 
+        }
+    }
+    )
+    
+
+    })
     resposta.render('home')
 })
 
